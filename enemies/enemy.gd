c0123ac5d68fd1925c0d70098e117877bb144218
atomjin <kazukiryouto@gmail.com>
2024-10-29 01:54:09 +0700

    sheep mons

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -10,18 +10,35 @@ extends Node2D
 @onready var destroyed_component:  = $DestroyedComponent as DestroyedComponent
 @onready var score_component:  = $ScoreComponent as ScoreComponent
 
+@export var player: Node2D
+@export var move_speed: float = 100.0 
 
-func _ready()->void:
-	stats_component.no_health.connect(func():
-		score_component.adjust_score()
-		)
-	
+func _ready() -> void:
+	# Connect signals
+	stats_component.no_health.connect(on_no_health)
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
-	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
-		scale_component.tween_scale()
-		flash_component.flash()
-		shake_component.tween_shake()
-	)
-	stats_component.no_health.connect(queue_free)
+	hurtbox_component.hurt.connect(on_hurt)
 	hitbox_component.hit_hurtbox.connect(destroyed_component.destroy.unbind(1))
+
+func _process(delta: float) -> void:
+	move_toward_player(delta)
+
+# Function to handle enemy moving toward player
+func move_toward_player(delta: float) -> void:
+	if player:
+		var direction = (player.global_position - global_position).normalized()
+		position += direction * move_speed * delta
+
+# Function called when health reaches zero
+func on_no_health() -> void:
+	score_component.adjust_score()
+	queue_free()
+
+# Function called when enemy is hurt
+func on_hurt(hitbox: HitboxComponent) -> void:
+	scale_component.tween_scale()
+	flash_component.flash()
+	shake_component.tween_shake()
+	
+
  
